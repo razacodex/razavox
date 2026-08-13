2a2fbaea6a22623805493ab3814e3f54ffbe460f
Mushahid Raza <mushahidtech2007@gmail.com>
2026-08-13 06:22:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'razavox-v1';
+const CACHE_NAME = 'razavox-v2';
+
 const urlsToCache = [
   './',
   './index.html',
@@ -7,15 +8,37 @@ const urlsToCache = [
   'https://cdn.jsdelivr.net/npm/lamejs@1.2.1/lame.min.js'
 ];
 
+// Install new Service Worker immediately
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Delete old caches and activate immediately
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Serve cached files, otherwise fetch from network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
-
